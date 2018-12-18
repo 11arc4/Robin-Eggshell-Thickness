91d8251385316dc30bafcc6d8a5ed869009d9a44
11arc4 <11arc4@queensu.ca>
2018-12-18 15:05:26 -0500

    Decisions about what to control for in later analyses.

--- Eggshell thickness data exploration.R
+++ Eggshell thickness data exploration.R
@@ -5,10 +5,6 @@ library(summarytools)
 library(lme4)
 
 shell <- read.csv("file:///C:/Users/11arc/Documents/Montogomerie Work/Robins/GoodThickness.csv")
-summarytools(shell)
-dfsummary(shell2)
-
-names(shell)
 
 shell2 <- shell %>% 
   select(-"NOTESLP", -"NOTESPE", -"KIT", -"SampleID",-AGEATSAM, -"yolkmass", -"Yolk_sample_mass", -Tng_yolkg, -Yolk_mass, -YolkTug, -fem.scaled.mass, -ECTO.s, -CONDIT) 
@@ -22,13 +18,15 @@ colnames(shell2) <- c("NestID", "EggLetter", "Year", "Treatment",
                       "Carotenoid_ugml", "Carotenoid_ugg", "Carot_ugyolk", "T_concentration", 
                       "FcaughtDOY", "FemaleAge", "Ectoparasites", "Mass_female", 
                       "Tarsus_female", "YellowAreaScore_female", "ColorRAchieved_female", 
-                      "Lum_egg_s", "HPhi_egg_s", "RVec_egg_s", "Thickness_egg"
+                      "Lum_egg_s", "HPhi_egg_s", "RVec_egg_s", "Thickness_shell"
                       )
 
 shell3 <- shell2 %>%
   group_by(NestID) %>% 
   mutate(ClutchMass_shell=sum(Mass_shell), 
-         Ectoparasites2 = ifelse(Ectoparasites>0, "YES", "NO")) 
+         Ectoparasites2 = ifelse(Ectoparasites>0, "YES", "NO"), 
+         EggAge = SampleDOY-LayDOY, 
+         Mass_yolk= ifelse(Mass_yolk>3, NA, Mass_yolk)) 
 
 ###Calculate female body condition as residual body mass in Mass ~ Tarsus
 Female <- shell3 %>% 
@@ -39,7 +37,7 @@ Female <- shell3 %>%
 residmod <- lm(Mass_female ~ Tarsus_female , data=Female)
 plot(residmod) #looks flawless
 Female$ResidMass_female <- resid(residmod, "pearson")
-shell2 <- merge(shell3, Female, all.x = T)
+shell3 <- merge(shell3, Female, all.x = T)
 
 rm(residmod, Female)
 
@@ -54,20 +52,83 @@ corrplot(m, p.mat=res1$p, sig.level = 0.05, insig="blank")
 ####Are shells thinner when there are more eggs in the clutch? 
 hist(shell2$ClutchSize, breaks=4) 
 # we should consider dropping all clutches that don't have 3-4 eggs since there are so few. 
-shell3 <- shell2 %>% group_by(NestID) %>% filter(ClutchSize %in% c(3,4)& n()>1) 
+shell4 <- shell3 %>% group_by(NestID) %>% filter(ClutchSize %in% c(3,4) )
 
 
 
 
 
 #########################################################
-#Which variables should we control for?
-shell3 <- shell2 %>% group_by(NestID) %>% filter(CLTCHSIZ %in% c(3,4)& n()>1) 
+#Which variables should we see if we need to control for them? E.g., things that we hope don't matter. 
 
-#Clutch size ()
+#Year, Treatment, IncStage (SampleDOY-LayDOY) 
+mod_year <- lm(Thickness_shell~factor(Year), data=shell4)
+plot(mod_year)
+summary(mod_year)
+anova(mod_year)
+#No year differences
+
+mod_treatment <- lm(Thickness_shell~Treatment, data=shell4)
+plot(mod_treatment)
+summary(mod_treatment)
+anova(mod_treatment)
+#No treatment differences in shell thickness
+
+mod_incstage <- lm(Thickness_shell~IncStage, data=shell4)
+plot(mod_incstage)
+summary(mod_incstage)
+anova(mod_incstage)
+#No age of egg at sampling differences, somewhat surprised because there is a
+#long tail of egg ages. 
+hist(shell4$IncStage)
+
+
+#Should still check to see if Inc Stage influenced  egg qualities (mass, carotenoids, testosterone)
+cmod_yolk <- lm(Mass_yolk~IncStage, data=shell4)
+plot(cmod_yolk) #saw some outliers on the histogram, now removed those above and it's much better
+summary(cmod_yolk)
+anova(cmod_yolk)
+#Yes it really does. We will need to control for this....
+
+
+cmod_eggmass <- lm(Mass_egg~IncStage, data=shell4)
+plot(cmod_eggmass) #fits perfectly
+summary(cmod_eggmass)
+anova(cmod_eggmass)
+#Nope no need to control for how old the eggs where when looking at egg mass
+
+
+
+cmod_alb <- lm(Mass_albumin~IncStage, data=shell4)
+plot(cmod_alb) #not really normal even a little bit
+summary(cmod_eggmass)
+anova(cmod_eggmass)
+#Don't need to control for age of eggs in albumin mass (ignoring normality issues)
+
+
+cmod_carot <- lm(Carot_ugyolk~IncStage, data=shell4)
+plot(cmod_carot) #Pretty good fit
+summary(cmod_carot)
+anova(cmod_carot)
+#Must control for age of egg when analysing carotenoids in yolk
+
+cmod_T <- lm(log(T_concentration)~IncStage, data=shell4)
+plot(cmod_T) #Pretty good fit
+summary(cmod_T)
+anova(cmod_T)
+#Should control for age of egg when analysing Testosterone concentration in yolk
 
 
 
 
-#Female body conditio
+#####Do higher quality females have higher quality eggs?
+#Indicators of Female quality: female body mass, yellowAreaScore,
+#colorRAchieved, FAge, Ectoparasites (Y/N)
+      ##Maybe test against the total shell mass as well
 
+#Indicators of higher quality eggs (presumably laid by higher quality females):
+#Egg volume, strength, mass of yolk, albumen, carotenoid in yolk, Testosterone
+#(?)
+
+#Do eggs that need to hatch faster have thinner eggs?
+#Lay order (presumed or otherwise), layDOY
