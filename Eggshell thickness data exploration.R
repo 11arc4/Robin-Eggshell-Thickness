6f3e8de56087cd16c095acc526eb49fe01b62e5f
11arc4 <11arc4@queensu.ca>
2018-12-21 15:29:58 -0500

    Trying (and failing) to sort out incubation times. IncStage is based on candling not known data, so may need to use something else.

--- Eggshell thickness data exploration.R
+++ Eggshell thickness data exploration.R
@@ -3,16 +3,18 @@ library(tidyverse)
 library(corrplot)
 library(lme4)
 library(lmerTest)
+library(stats)
+
 
 shell <- read.csv("file:///C:/Users/11arc/Documents/Montogomerie Work/Robins/Thickness Dataset (reduced).csv")
 
 shell2 <- shell %>% 
-  select(-"NOTESLP", -"NOTESPE", -"KIT", -"SampleID",-AGEATSAM, -"Yolk_sample_mass", -Tng_yolkg,-Tng_yolkmg, -YolkTug, -CONDIT) 
+  select(-"NOTESLP", -"NOTESPE", -"KIT", -"SampleID", -"Yolk_sample_mass", -Tng_yolkg,-Tng_yolkmg, -YolkTug, -CONDIT) 
 
 
 colnames(shell2) <- c("NestID", "EggLetter", "Year", "Treatment", 
                       "FemaleID", "MaleID", "ClutchSize", "SampleDOY", 
-                      "IncStage", "LayDOY", "LayOrder_est", "LayOrder_known",
+                      "IncStage", "LayDOY", "EggAge", "LayOrder_est", "LayOrder_known",
                       "Mass_egg", "Length_egg", "Width_egg", "Volume_egg",
                       "Mass_yolk", "Mass_albumen", "Mass_shell", "MeanStrength", 
                       "Carotenoid_ugml", "Carotenoid_ugg", "Carot_ugyolk", "T_concentration", 
@@ -21,7 +23,7 @@ colnames(shell2) <- c("NestID", "EggLetter", "Year", "Treatment",
                       "RVec_female", "Lum_female", "ScaledBodyMass_female", "HTheta_egg", 
                       "HPhi_egg", "RVec_egg", "RMax_egg", "RAchieved_egg", 
                       "Lum_egg", "Thickness_shell"
-                      )
+)
 
 shell2 <- shell2 %>%
   group_by(NestID) %>% 
@@ -43,13 +45,6 @@ Female$ResidMass_female <- resid(residmod, "pearson")
 shell2 <- merge(shell2, Female, all.x = T, all.y=T)
 
 rm(residmod, Female)
-
-
-
-m <- cor(shell2[,c(7:35, 37)], use="pairwise.complete.obs")
-res1 <- cor.mtest(shell2[,7:37],  conf.level = .95)
-corrplot(m, p.mat=res1$p, sig.level = 0.05, insig="blank")
- 
 
 
 ####Are shells thinner when there are more eggs in the clutch? 
@@ -77,47 +72,84 @@ summary(mod_treatment)
 anova(mod_treatment)
 #No treatment differences in shell thickness
 
-mod_incstage <- lm(Thickness_shell~IncStage, data=shell2)
+mod_incstage <- lm(Thickness_shell~EggAge, data=shell2)
 plot(mod_incstage)
 summary(mod_incstage)
 anova(mod_incstage)
-
 #No age of egg at sampling differences, somewhat surprised because there is a
 #long tail of egg ages. 
-hist(shell2$IncStage)
+hist(shell2$EggAge)
 
 
 #Should still check to see if Inc Stage influenced  egg qualities (mass, carotenoids, testosterone)
-cmod_yolk <- lm(Mass_yolk~IncStage, data=shell2)
+cmod_yolk <- lm(Mass_yolk~EggAge, data=shell2 %>% filter(Mass_yolk<2.2 & EggAge<6))
 plot(cmod_yolk) #saw some outliers on the histogram, now removed those above and it's much better
 summary(cmod_yolk)
 anova(cmod_yolk)
-#Yes it really does. We will need to control for this....
+#Yes it really does. We will need to control for this....Do need to remove the
+#outliers from this analysis as well probably.
+ggplot(shell2, aes(x=EggAge, y=Mass_yolk))+
+  geom_point(color=ifelse(shell2$Mass_yolk < 2.2, "black", "red"))+
+  geom_smooth(method="lm", data=shell2 %>% filter(Mass_yolk<2.2 & EggAge<6), color="black")+
+  geom_smooth(method="lm", color="red")+
+  theme_classic()
+ggplot(shell2, aes(x=IncStage, y=Mass_yolk))+
+  geom_point(color=ifelse(shell2$Mass_yolk < 2.2, "black", "red"), shape=1)+
+  geom_smooth(method="lm", data=shell2 %>% filter(Mass_yolk<2.2), color="black")+
+  geom_smooth(method="lm", color="red")+
+  geom_smooth(method="lm", data=)
+  theme_classic()
 
 
-cmod_eggmass <- lm(Mass_egg~IncStage, data=shell2)
+
+cmod_eggmass <- lm(Mass_egg~EggAge, data=shell2)
 plot(cmod_eggmass) #fits perfectly
 summary(cmod_eggmass)
 anova(cmod_eggmass)
 #Nope no need to control for how old the eggs where when looking at egg mass
 
-cmod_alb <- lm(Mass_albumen~IncStage, data=shell2)
+
+
+cmod_alb <- lm(Mass_albumen~EggAge, data=shell2)
 plot(cmod_alb) #not really normal even a little bit
 summary(cmod_eggmass)
 anova(cmod_eggmass)
 #Don't need to control for age of eggs in albumen mass (ignoring normality issues)
+ggplot(shell2, aes(x=EggAge, y=Mass_albumen))+
+  geom_point()+
+  geom_smooth(method="lm", color="red")+
+  theme_classic()
 
-cmod_carot <- lm(Carot_ugyolk~IncStage, data=shell2)
+cmod_carot <- lm(Carot_ugyolk~EggAge, data=shell2[-c(6,80,114),]) #removed leverage points
 plot(cmod_carot) #Pretty good fit
 summary(cmod_carot)
 anova(cmod_carot)
-#Must control for age of egg when analysing carotenoids in yolk
+#Must control for age of egg when analysing carotenoids in yolk (even after removing leverage points)
 
-cmod_T <- lm(log(T_concentration)~IncStage, data=shell2)
+ggplot(shell2, aes(x=EggAge, y=Carot_ugyolk))+
+  geom_point( shape=1)+
+  geom_smooth(method="lm")+
+  geom_smooth(method="lm", data=shell2[-c(6,80,114),])+
+  theme_classic()
+
+cmod_T <- lm(T_concentration~EggAge, data=shell2)
 plot(cmod_T) #Pretty good fit
 summary(cmod_T)
 anova(cmod_T)
-#Should control for age of egg when analysing Testosterone concentration in yolk
+#Don't need to control for eggAge in testosterone concentration
+
+ggplot(shell2, aes(x=EggAge, y=T_concentration))+
+  geom_point( shape=1)+
+  geom_smooth(method="lm")+
+  theme_classic()
+
+ggplot(shell2%>% filter(T_concentration<20), aes(x=IncStage, y=T_concentration))+
+  geom_point( shape=1)+
+  geom_smooth(method="lm")+
+  xlim(c(0,2))+
+  theme_classic()
+
+
 ###########################################################################################
 #Do clutch level traits affect eggshell thickness?
 mod_clutch <- lmer(Thickness_shell ~ ClutchSize + (1|NestID), data=shell2)
@@ -146,7 +178,7 @@ anova(mod_order)
 ###########################################################################################
 #####Do higher quality females lay eggs with thicker eggshells?
 
-mod_femage <- lmer(Thickness_shell ~ FemaleAge + (1|NestID), data=shell2) #No need for random
+mod_femage <- lmer(Thickness_shell ~ factor(FemaleAge) + (1|NestID), data=shell2) #No need for random
 #mod_femage <- lm(Thickness_shell ~ FemaleAge, data=shell2)
 plot(mod_femage)
 summary(mod_femage)
@@ -270,68 +302,22 @@ anova(mod_carot)
 #Once you control for how old the egg is, carotenoids don't predict shell thickness. 
 
 
-
-
-
-
-#Egg color 
-mod_eggcolor <- lmer(Thickness_shell ~ HTheta_egg + HPhi_egg + RVec_egg + (1|NestID), data=shell2)
-summary(mod_eggcolor)
-anova(mod_eggcolor)
-plot(mod_eggcolor)#Doesn't fit great but not so bad
-#RVec might correlate weakly with thickness but that's about it. 
-
-ggplot(shell2 %>% filter(HTheta_egg>2.15 & Thickness_shell<=0.14), aes(x=HTheta_egg, y=Thickness_shell))+
-  geom_point(shape=1)+
-  geom_smooth(method="lm")
-
-ggplot(shell2, aes(x=HPhi_egg_s, y=Thickness_shell))+
-  geom_point()+
-  geom_smooth(method="lm")
-
-
-ggplot(shell2, aes(x=RVec_egg_s, y=Thickness_shell))+
-  geom_point()+
-  geom_smooth(method="lm")
-
-
-
-
-models <- c(mod_clutch, mod_date, mod_order, 
-            mod_femage, mod_femcondition, mod_ecto, mod_bill, 
-            mod_volume, mod_yolk, mod_alb, mod_eggcolor, mod_T, mod_carot)
-p <- rep(NA, length(models))
-i=0
-for(mod in models){
-  i=1+i
-  p[i] <- anova(mod)$`Pr(>F)`
-}
-
-p.adjust(p, method="fdr")
-
-
-
-
-
-
-
 #PCA for egg color. 
-library(stats)
+#https://stats.stackexchange.com/questions/59213/how-to-compute-varimax-rotated-principal-components-in-r
 
-
-PCA_color <- prcomp( shell2[,36:40], 
+PCA_color <- prcomp( shell2[,c(37:39, 41:42)], 
                center=T, 
                scale=T, 
                retx=T)
 
-#plot(TopEggPCA, type="lines")
+plot(PCA_color, type="lines")
 ncomp<-2
 rawLoadings     <- PCA_color$rotation[,1:ncomp] %*% diag(PCA_color$sdev, ncomp, ncomp)
 rotatedLoadings <- varimax(rawLoadings)$loadings
 invLoadings     <- t(pracma::pinv(rotatedLoadings))
-rownames(invLoadings) <- colnames(PCA_color[36:40])
+rownames(invLoadings) <- colnames(shell2[,c(37:39, 41:42)])
 
-scores <- scale(shell2[,36:40]) %*% invLoadings
+scores <- scale(shell2[,c(37:39, 41:42)]) %*% invLoadings
 
 ####Calculate PCs for Top of Egg
 shell2$PC1 <-  scores[,1] #50% of variance
@@ -344,9 +330,33 @@ hist(resid(mod_colorpc))
 anova(mod_colorpc)
 summary(mod_colorpc)
 
+
+mod_pc <- lm(PC1 ~ EggAge, data=shell2) 
+plot(mod_pc)#Doesn't fit well
+anova(mod_pc)
+ggplot(shell2, aes(x=EggAge, y=PC1))+
+  geom_point()
+  geom_smooth(method="lm")
+#PC1 should be controlled for egg age
+
+mod_pc <- lm(PC2 ~ EggAge, data=shell2[-c(142,114),])
+plot(mod_pc)#Doesn't fit well
+anova(mod_pc)
+ggplot(shell2[-c(142,114),], aes(x=EggAge, y=PC2))+
+  geom_point()+
+  geom_smooth(method="lm")
+
 ggplot(shell2, aes(x=PC1, y=Thickness_shell))+
   geom_point(shape=1)+
   geom_smooth(method="lm")
 
 
+mod_colorpc2 <- lmer(Thickness_shell ~ PC2 + (1|NestID), data=shell2) #Don't need random effect
+plot(mod_colorpc2)#Doesn't fit well
+hist(resid(mod_colorpc2))
+anova(mod_colorpc2)
+summary(mod_colorpc2)
 
+ggplot(shell2, aes(x=PC2, y=Thickness_shell))+
+  geom_point(shape=1)+
+  geom_smooth(method="lm")
